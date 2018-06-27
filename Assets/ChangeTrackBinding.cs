ca86826c1cfbfc92db369b45b613ef201256f83d
John (Jihyun) Oh <johno@unity3d.com>
2018-06-27 15:20:09 +0900

    initial commit

---
+++ Assets/ChangeTrackBinding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class ChangeTrackBinding : MonoBehaviour {
+
+    public PlayableDirector director;
+    public GameObject targetInstance;
+    public string trackName;
+
+    public void ChangeInstance () {
+        TimelineAsset timelineAsset = (TimelineAsset)director.playableAsset;
+        if (timelineAsset == null)
+            return;
+        TrackAsset track = null;
+        if( trackName.Length> 0) {
+            foreach( TrackAsset t in timelineAsset.GetOutputTracks()) {
+                Debug.Log(t.name);
+                if (t.name.Equals(trackName))
+                    track = t;
+            }
+        } else {
+            track = timelineAsset.GetOutputTrack(0);
+        }
+        if( track != null) {
+            if( track.GetType() == typeof(AnimationTrack))
+                director.SetGenericBinding(track, targetInstance.GetComponent<Animator>());
+            else
+                director.SetGenericBinding(track, targetInstance);
+        }
+            
+	}
+	
+    private void Start()
+    {
+        ChangeInstance();
+    }
+
+}
